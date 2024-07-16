635aa777db8a5653b1973b85bd694ce857518bad
Huyen35844 <thuhuyen35844@gmail.com>
2024-07-16 12:36:17 +0700

    feature_sign_up

--- App.tsx
+++ App.tsx
@@ -4,11 +4,13 @@ import {
   SafeAreaView, StatusBar, StyleSheet,
 } from 'react-native';
 import Navigator from './src/navigator';
+import FlashMessage from 'react-native-flash-message';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaView style={styles.container}>
       <Navigator />
+      <FlashMessage position="top" />
     </SafeAreaView>
   );
 }
